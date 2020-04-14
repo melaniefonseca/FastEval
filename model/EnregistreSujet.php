564074077f0a0ae51c4b31557d463f55c6ade093
melaniefonseca <melanie93fonseca@gmail.com>
2020-04-14 18:34:22 +0200

    correction + plan site

--- model/EnregistreSujet.php
+++ model/EnregistreSujet.php
@@ -4,10 +4,10 @@ require_once ("../include/PdoFastEval.php");
 
 $pdo = PdoFastEval::getPdoFastEval();
 
-if(isset($_POST["libelle_sujet"]) && isset($_POST["sujet"]) && isset($_POST["idevaluation"])) {
+if(isset($_POST["libelle_sujet"]) && isset($_FILES["sujet"]) && isset($_POST["idevaluation"])) {
     
     $idEvaluation = $_POST["idevaluation"];
-    $libelleunique = $_POST["libelle_sujet"];
+    $libelleunique = str_replace(" ", "", $_POST["libelle_sujet"]);
     
     chdir('../Sujets/');
     
@@ -19,12 +19,13 @@ if(isset($_POST["libelle_sujet"]) && isset($_POST["sujet"]) && isset($_POST["ide
         else {
             $dossier = getcwd().'/';
             $fichier = basename($_FILES['sujet']['name']);
-
+            
             $fichierExiste = file_exists($dossier . $libelleunique.'.txt');
-
+            
             if (!$fichierExiste) {
                 move_uploaded_file($_FILES['sujet']['tmp_name'], $dossier . $libelleunique.'.txt');
-                $chemin = getcwd() . "\\" . $libelleunique . ".txt";
+                $chemin = str_replace("\\", "/", getcwd() . "\\" . $libelleunique . ".txt");
+                
                 $pdo->insertSujet($libelleunique, $chemin, $idEvaluation);
             }
         }
